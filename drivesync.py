1a8a87d49c8f463e18683cd5035eefe664c11ffc
Fotis Antonatos <fotis.antonatos7@gmail.com>
2019-09-25 11:06:10 -0400

    Formatting

--- drivesync.py
+++ drivesync.py
@@ -1,18 +1,23 @@
 from os.path import exists
+from sys import exit
 import time
 
 '''
     Fotis Antonatos
     24 Sep, 2019
     
+    Last Modified: 25 Sep, 2019
 '''
 
 # Data
 
-#     look for drives with the id "debug1" inside the drivesync.inf file
-#     "documents" is a built-in link to the user's documents library
-commands = [ "sync debug1 with directory documents" 
-            "sync debug2 with subdirectory .../Pictures/SummerVacation1999"]
+#           look for drives with the id "debug1" inside the drivesync.inf file
+#           "documents" is a built-in link to the user's documents library
+test_commands = [   "sync debug1 with directory documents" 
+                    "sync debug2 with subdirectory .../Pictures/SummerVacation1999"]
+test_drive = "G"
+
+DEBUG = False
 
 
 # Functions
@@ -20,8 +25,8 @@ commands = [ "sync debug1 with directory documents"
 # Returns true if drive is set up for sync
 def isSyncEnabled(driveLetter):
     if exists(driveLetter + ':\\drivesync.inf'):
-        return True;
-    return False;
+        return True
+    return False
 
 # Returns list of all drives on the system
 def getDrives():
@@ -31,7 +36,7 @@ def getDrives():
             drives.append(chr(drive))
     return drives
 
-def onDriveInserted(driveLetter):
+def onDriveInsertion(driveLetter):
     sync = isSyncEnabled(driveLetter)
     
     return sync
@@ -50,7 +55,9 @@ print('drivesync now monitoring drives')
 previousDrives = getDrives()
 while True:
     currentdrives = getDrives()
-    print(currentdrives)
+    
+    if DEBUG:
+        print(str(currentdrives) + " testing against last iteration")
     
     # Run on drive insertion
     if currentdrives.__len__() > previousDrives.__len__():
@@ -59,14 +66,14 @@ while True:
     # Run on drive removal
     if currentdrives.__len__() < previousDrives.__len__():
         print("Drive(s) Removed.")
-        
+    
     time.sleep(1)
-    previousDrives = currentdrives;
+    previousDrives = currentdrives
     
     
 
 # Close Program
 print('drivesync closing')
-exit();
+sys.exit(0)
 
 
